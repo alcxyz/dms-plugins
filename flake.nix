b44166fc3913e132e6a533b85dbe2660c5adc50d
alcxyz <me@alc.no>
2026-04-23 00:37:10 +0200

    Initial meta-flake bundling all DMS plugin sources

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Unified source bundle for DankMaterialShell plugins";
+
+  inputs = {
+    worldclock = {
+      url = "github:alcxyz/WorldClock";
+      flake = false;
+    };
+    calculator = {
+      url = "github:alcxyz/DankCalculator";
+      flake = false;
+    };
+    quicksearch = {
+      url = "github:alcxyz/DankQuickSearch";
+      flake = false;
+    };
+    vault = {
+      url = "github:alcxyz/DankVault";
+      flake = false;
+    };
+    translate = {
+      url = "github:alcxyz/DankTranslate";
+      flake = false;
+    };
+    spotify = {
+      url = "github:alcxyz/DankSpotify";
+      flake = false;
+    };
+  };
+
+  outputs =
+    { self, ... }@inputs:
+    {
+      srcs = builtins.removeAttrs inputs [ "self" ];
+    };
+}
